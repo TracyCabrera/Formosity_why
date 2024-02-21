294940de03a60717ea6da057a67700021b645c70
Tracy <trfr.cabrera.up@phinmaed.com>
2024-02-21 17:35:30 +0800

    2nd

---
+++ app/src/main/java/com/application/formosity_project/account_page.kt
@@ -0,0 +1,11 @@
+package com.application.formosity_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class account_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_page)
+    }
+}
---
+++ app/src/main/java/com/application/formosity_project/update_account.kt
@@ -0,0 +1,11 @@
+package com.application.formosity_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class update_account : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_account)
+    }
+}
